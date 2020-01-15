a7dfc9bf9612755cb666a62c50ae1a87f2f35ab9
astounding-spooling-2354 <nlashner@gmail.com>
2020-01-15 03:07:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// add solution here
+learn open js-beatles-loops-lab
+
+function theBeatlesPlay(arr1, arr2) {
+  let newArr = []
+  for (let i = 0; i < arr1.length; i++){
+    let member = arr1[i]
+    let instrument = arr2[i]
+    newArr.push(member + ' plays ' + instrument)
+  }
+  return newArr
+}
+
+function johnLennonFacts(array){
+  let newArr = []
+  let i =0
+  while (i < array.length){
+    newStr = array[i] + '!!!'
+    newArr.push(newStr)
+    i++
+  }
+  return newArr
+}
